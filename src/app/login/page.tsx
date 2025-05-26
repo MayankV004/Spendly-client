36b935547256901715324ec423d6d0d79e8a0ea5
MayankV004 <abcdef.msverma@gmail.com>
2025-05-27 01:55:26 +0530

    Initial Commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from '@/components/auth/LoginForm';
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+        <LoginForm/>
+    </div>
+  )
+}
+
+export default Login;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+import { SignUpForm } from '@/components/auth/SignUpForm';
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+
+        <SignUpForm/>
+    </div>
+  )
+}
+
+export default SignUp;
